15737165bb643b5a1d3e7ad553db870035c2977c
angelchiav <achiavicuna@gmail.com>
2025-06-18 03:49:59 -0400

    Update models.py

--- users/models.py
+++ users/models.py
@@ -40,6 +40,7 @@ class Product(models.Model):
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
